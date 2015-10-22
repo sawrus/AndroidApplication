f08060fc039a99bfe6be9be68ca99f13720052bf
EC2 Default User <ec2-user@ip-172-31-47-213.us-west-2.compute.internal>
2015-10-22 13:20:14 +0000

    added: server

---
+++ server/testDb.js
@@ -0,0 +1,9 @@
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+
+var url = 'mongodb://localhost:20017/spy';
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected correctly to server.");
+  db.close();
+});

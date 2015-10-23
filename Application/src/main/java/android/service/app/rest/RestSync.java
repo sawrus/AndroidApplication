1836ba5015d44d788bc832cb2f124b61b3ab85e8
Evgeny Isaev <sawrus@gmail.com>
2015-10-23 13:18:33 +0400

    small update

--- Application/src/main/java/android/service/app/rest/RestSync.java
+++ Application/src/main/java/android/service/app/rest/RestSync.java
@@ -1,9 +1,12 @@
 package android.service.app.rest;
 
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.service.app.db.DatabaseHelper;
 
+@TargetApi(Build.VERSION_CODES.CUPCAKE)
 public class RestSync<Input> extends AsyncTask<Input, Void, DatabaseSyncOutput>
 {
     private final DatabaseHelper localDatabase;
@@ -20,7 +23,7 @@ public class RestSync<Input> extends AsyncTask<Input, Void, DatabaseSyncOutput>
     @SafeVarargs
     protected final DatabaseSyncOutput doInBackground(Input... voids)
     {
-        //todo
+        //todo, you can use the part of implementation from AndroidApplication\Application\src\main\java\android\service\app\db\mongo\MongoClient.java
 
         return null;
     }
